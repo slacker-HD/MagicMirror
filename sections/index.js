2c440d2a0dbd6cca73fb51ec296628f0d2915648
slacker_HD <hudi@linuxmail.org>
2019-07-09 22:00:22 +0800

    add branch

---
+++ sections/index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+import {
+    app,
+    BrowserWindow,
+} from 'electron';
+
+// const Parser = require('rss-parser');
+
+// const parser = new Parser();
+let mainWindow;
+
+const createWindow = () => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 480,
+    });
+    mainWindow.setMenu(null);
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+    mainWindow.webContents.openDevTools();
+    mainWindow.setFullScreen(true);
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+
+const gotTheLock = app.requestSingleInstanceLock();
+if (!gotTheLock) {
+    app.quit();
+} else {
+    app.on('second-instance', (commandLine, workingDirectory) => {
+        console.log(commandLine + workingDirectory);
+        if (mainWindow) {
+            if (mainWindow.isMinimized()) {
+                mainWindow.restore();
+            }
+            mainWindow.focus();
+        }
+    });
+}
+
+// (async() => {
+
+//     const feed = await parser.parseURL('http://www.hudi.site/atom.xml');
+//     console.log(feed.title);
+
+//     feed.items.forEach(item => {
+//         console.log(item.title + ':' + item.link);
+//     });
+
+// })();
