b1c7e25f3f8c3a8e9df7079ee13adb7a85edc785
slacker_HD <hudi@linuxmail.org>
2019-07-09 23:20:32 +0800

    fix config error

--- renderer-process/render.js
+++ renderer-process/render.js
@@ -4,15 +4,9 @@
 
 function blockdrag() {
     const holder = document.getElementById('drag-file');
-    holder.ondragover = () => {
-        return false;
-    };
-    holder.ondragleave = () => {
-        return false;
-    };
-    holder.ondragend = () => {
-        return false;
-    };
+    holder.ondragover = () => false;
+    holder.ondragleave = () => false;
+    holder.ondragend = () => false;
     holder.ondrop = (e) => {
         e.preventDefault();
         // for (let f of e.dataTransfer.files) {
